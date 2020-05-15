c59fe5b9cd9c032f954da2e0b872fca396e18d94
Eric Hamiter <ehamiter@gmail.com>
2020-05-15 08:54:19 -0500

    Fix possible null values

--- js/index.js
+++ js/index.js
@@ -37,28 +37,28 @@ const getRiverInfo = (data) => {
 
 function getDischargeInfo(discharge) {
     let result = null;
-    if (discharge < 50) {
+    if (discharge <= 50) {
         result = 'The river is running super duper slow.';
     }
-    if (50 < discharge && discharge < 150) {
+    if (50 < discharge && discharge <= 150) {
         result = 'The river is running pretty slow today.';
     }
-    if (150 < discharge  && discharge  < 300) {
+    if (150 < discharge && discharge  <= 300) {
         result = 'The river is running a little slow today.';
     }
-    if (300 < discharge  && discharge  < 800) {
+    if (300 < discharge && discharge  <= 800) {
         result = 'The river is running great today.';
     }
-    if (800 < discharge  && discharge  < 1100) {
+    if (800 < discharge && discharge  <= 1100) {
         result = 'The river is running fast today.';
     }
-    if (1100 < discharge  && discharge  < 2000) {
+    if (1100 < discharge && discharge  <= 2000) {
         result = 'The river is running very fast today.';
     }
-    if (2000 < discharge  && discharge  < 4000) {
+    if (2000 < discharge && discharge  <= 4000) {
         result = 'The river is running extremely fast today. Be careful.';
     }
-    if (discharge  > 4000) {
+    if (discharge > 4000) {
         result = 'The river is probably running too fast to kayak today.';
     }
     return result    
@@ -66,31 +66,31 @@ function getDischargeInfo(discharge) {
 
 function getGageInfo(gage) {
     let result = null;
-    if (gage < 1.5) {
+    if (gage <= 1.5) {
         result ='It\'s bone dry and not possible to kayak.';
     }
-    if (1.5 < gage && gage < 1.9) {
+    if (1.5 < gage && gage <= 1.9) {
         result = 'You\'ll probaby have to portage some.';
     }
-    if (1.9 < gage && gage < 2.3) {
+    if (1.9 < gage && gage <= 2.3) {
         result = 'The water level is a little lower than average.';
     }
-    if (2.3 < gage && gage < 2.8) {
+    if (2.3 < gage && gage <= 2.8) {
         result = 'The water level is right around the average.';
     }
-    if (2.8 < gage && gage < 3.5) {
+    if (2.8 < gage && gage <= 3.5) {
         result = 'The water level is great, you should be fine.';
     }
-    if (3.5 < gage && gage < 4.0) {
+    if (3.5 < gage && gage <= 4.0) {
         result = 'The water level is a little high.';
     }
-    if (4.0 < gage && gage < 4.5) {
+    if (4.0 < gage && gage <= 4.5) {
         result = 'Be careful, the water is higher than normal.';
     }
-    if (4.5 < gage && gage < 5.0) {
+    if (4.5 < gage && gage <= 5.0) {
         result = 'Water is very high. Might be risky.';
     }
-    if (5.0 < gage && gage < 6.0) {
+    if (5.0 < gage && gage <= 6.0) {
         result = 'Probably not a good idea to kayak today.';
     }
     if (gage > 6.0) {
