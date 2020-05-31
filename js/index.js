69c2bda6150f675eb02f6ba2a7d2d68f65e4be62
Eric Hamiter <ehamiter@gmail.com>
2020-05-31 12:23:40 -0500

    Add water temperature information

--- js/index.js
+++ js/index.js
@@ -1,21 +1,18 @@
-/* 
-* You can look up Site Numbers at the USGS here:
-* https://waterdata.usgs.gov/nwis/inventory
-* Keep in mind it has to have gage and discharge data,
-* and even then, it still may not parse correctly. 
-*/
-const site = '03433500';  // Harpeth River at Bellevue, Tn
-
 const main = document.getElementById('main');
 main.innerHTML = '<p>Loading...</p>';
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const url = `https://waterservices.usgs.gov/nwis/dv/?format=json&sites=${site}&parameterCd=00060,00065`;
+const url = 'https://waterservices.usgs.gov/nwis/dv/?format=json&sites=03433500&parameterCd=00060,00065&siteStatus=all';
+const waterTempUrl = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=034324146&parameterCd=00010&siteStatus=all';
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => main.innerHTML = getRiverInfo(data));
+
+fetch(waterTempUrl)
+  .then((response) => response.json())
+  .then((waterTempData) => temp.innerHTML = getTempInfo(waterTempData));
 
 const getRiverInfo = (data) => {
   const siteName = data['value']['timeSeries'][0]['sourceInfo']['siteName'].split(' ')
@@ -34,6 +31,50 @@ const getRiverInfo = (data) => {
   `;
   return `${info}`;
 };
+
+const getTempInfo = (waterTempData) => {
+  let waterTemp = waterTempData['value']['timeSeries'][0]['values'][0]['value'][0]['value'];
+  waterTemp = (waterTemp * 9/5) + 32;
+  waterTemp = Math.round(waterTemp * 10) / 10;
+  const waterTempInfo = getWaterTempInfo(waterTemp);
+  const info = `<p class="lead">${waterTempInfo} (${waterTemp}º F)</p>`;
+  return `${info}`;
+};
+
+function getWaterTempInfo(waterTemp) {
+    let result = null;
+    if (waterTemp <= 45.0) {
+        result = 'The water temperature is super duper cold!';
+    }
+    if (45.0 < waterTemp && waterTemp <= 50.0) {
+        result = 'The water temperature is extremely cold!';
+    }
+    if (50.0 < waterTemp && waterTemp <= 55.0) {
+        result = 'The water temperature is very cold.';
+    }
+    if (55.0 < waterTemp && waterTemp <= 60.0) {
+        result = 'The water temperature is cold.';
+    }
+    if (60.0 < waterTemp && waterTemp <= 65.0) {
+        result = 'The water temperature is just a little bit cold.';
+    }
+    if (65.0 < waterTemp && waterTemp <= 70.0) {
+        result = 'The water temperature is pretty nice, just a touch chilly.';
+    }
+    if (70.0 < waterTemp && waterTemp <= 75.0) {
+        result = 'The water temperature is very nice.';
+    }
+    if (75.0 < waterTemp && waterTemp <= 80.0) {
+        result = 'The water temperature is super comfortable.';
+    }
+    if (80.0 < waterTemp && waterTemp <= 85.0) {
+        result = 'The water temperature is really warm.';
+    }
+    if (waterTemp > 85.0) {
+        result = 'The water temperature is almost like a hot tub!';
+    }
+    return result;
+}
 
 function getDischargeInfo(discharge) {
     let result = null;
@@ -61,7 +102,7 @@ function getDischargeInfo(discharge) {
     if (discharge > 4000) {
         result = 'The river is probably running too fast to kayak today.';
     }
-    return result    
+    return result;
 }
 
 function getGageInfo(gage) {
@@ -96,5 +137,5 @@ function getGageInfo(gage) {
     if (gage > 6.0) {
         result = 'The water is too damn high!';
     }
-    return result
+    return result;
 }
