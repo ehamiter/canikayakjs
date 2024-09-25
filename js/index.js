a9c900d719f82d197a1b533b7bba0afbe70f6f29
Eric Hamiter <ehamiter@gmail.com>
2024-09-25 17:37:46 -0500

    Update info if temp not working

--- js/index.js
+++ js/index.js
@@ -13,38 +13,41 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const waterTempUrl = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=034324146&parameterCd=00010&siteStatus=all';
 
     fetch(url)
-      .then((response) => response.json())
-      .then((data) => main.innerHTML = getRiverInfo(data));
+        .then((response) => response.json())
+        .then((data) => main.innerHTML = getRiverInfo(data));
 
     fetch(waterTempUrl)
-      .then((response) => response.json())
-      .then((waterTempData) => temp.innerHTML = getTempInfo(waterTempData));
+        .then((response) => response.json())
+        .then((waterTempData) => temp.innerHTML = getTempInfo(waterTempData));
 
     const getRiverInfo = (data) => {
-      const siteName = data['value']['timeSeries'][0]['sourceInfo']['siteName'].split(' ')
-        .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
-        .join(' ');
-      const discharge = data['value']['timeSeries'][0]['values'][0]['value'][0]['value'];
-      const gage = data['value']['timeSeries'][1]['values'][0]['value'][0]['value'];
-      const dischargeInfo = getDischargeInfo(discharge);
-      const gageInfo = getGageInfo(gage);
+        const siteName = data['value']['timeSeries'][0]['sourceInfo']['siteName'].split(' ')
+            .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
+            .join(' ');
+        const discharge = data['value']['timeSeries'][0]['values'][0]['value'][0]['value'];
+        const gage = data['value']['timeSeries'][1]['values'][0]['value'][0]['value'];
+        const dischargeInfo = getDischargeInfo(discharge);
+        const gageInfo = getGageInfo(gage);
 
-      const info = `
+        const info = `
         <h1 class="cover-heading">${siteName}</h1>
         <br />
         <p class="lead">${dischargeInfo} (Discharging at ${discharge} ft&sup3; per second)</p>
         <p class="lead">${gageInfo} (Gage height of ${gage} ft)</p>
       `;
-      return `${info}`;
+        return `${info}`;
     };
 
     const getTempInfo = (waterTempData) => {
-      let waterTemp = waterTempData['value']['timeSeries'][0]['values'][0]['value'][0]['value'];
-      waterTemp = (waterTemp * 9/5) + 32;
-      waterTemp = Math.round(waterTemp * 10) / 10;
-      const waterTempInfo = getWaterTempInfo(waterTemp);
-      const info = `<p class="lead">${waterTempInfo} (${waterTemp}º F)</p>`;
-      return `${info}`;
+        let waterTemp = waterTempData['value']['timeSeries'][0]['values'][0]['value'][0]['value'];
+        if (waterTemp == -999999) {
+            return `<p class="lead">Water temperature unknown; site for water temperature undergoing maintenance</p>`;
+        }
+        waterTemp = (waterTemp * 9 / 5) + 32;
+        waterTemp = Math.round(waterTemp * 10) / 10;
+        const waterTempInfo = getWaterTempInfo(waterTemp);
+        const info = `<p class="lead">${waterTempInfo} (${waterTemp}º F)</p>`;
+        return `${info}`;
     };
 
     function getWaterTempInfo(waterTemp) {
@@ -93,19 +96,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
         if (50 < discharge && discharge <= 150) {
             result = 'The river is running pretty slow today.';
         }
-        if (150 < discharge && discharge  <= 300) {
+        if (150 < discharge && discharge <= 300) {
             result = 'The river is running a little slow today.';
         }
-        if (300 < discharge && discharge  <= 800) {
+        if (300 < discharge && discharge <= 800) {
             result = 'The river is running great today.';
         }
-        if (800 < discharge && discharge  <= 1100) {
+        if (800 < discharge && discharge <= 1100) {
             result = 'The river is running fast today.';
         }
-        if (1100 < discharge && discharge  <= 2000) {
+        if (1100 < discharge && discharge <= 2000) {
             result = 'The river is running very fast today.';
         }
-        if (2000 < discharge && discharge  <= 4000) {
+        if (2000 < discharge && discharge <= 4000) {
             result = 'The river is running extremely fast today. Be careful.';
         }
         if (discharge > 4000) {
@@ -117,7 +120,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function getGageInfo(gage) {
         let result = null;
         if (gage <= 0.5) {
-            result ='It\'s bone dry and not possible to kayak.';
+            result = 'It\'s bone dry and not possible to kayak.';
         }
         if (0.5 < gage && gage <= 1.5) {
             result = 'You\'ll have to portage a lot.';
